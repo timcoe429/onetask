e015a17d844009b3a01ba4c3b009cadf9a62a78f
timcoe429 <timcoe429@gmail.com>
2025-06-27 11:34:43 -0400

    Update helper.js

--- public/utils/helper.js
+++ public/utils/helper.js
@@ -1,1 +1,8 @@
-
+// utils/helpers.js
+window.Utils = {
+    escapeHtml: function(text) {
+        const div = document.createElement('div');
+        div.textContent = text;
+        return div.innerHTML;
+    }
+};
