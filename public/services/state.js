4008593b678fede31a54b828dcef16dadd7b0f2f
timcoe429 <timcoe429@gmail.com>
2025-06-27 11:34:55 -0400

    Update state.js

--- public/services/state.js
+++ public/services/state.js
@@ -1,1 +1,22 @@
-
+// services/state.js
+window.StateManager = {
+    // Get focus mode from localStorage
+    getFocusMode: function() {
+        return parseInt(localStorage.getItem('focusMode') || '0');
+    },
+    
+    // Set focus mode in localStorage
+    setFocusMode: function(mode) {
+        localStorage.setItem('focusMode', mode.toString());
+    },
+    
+    // Get current project index for super focus mode
+    getCurrentProjectIndex: function() {
+        return parseInt(localStorage.getItem('currentProjectIndex') || '0');
+    },
+    
+    // Set current project index
+    setCurrentProjectIndex: function(index) {
+        localStorage.setItem('currentProjectIndex', index.toString());
+    }
+};
